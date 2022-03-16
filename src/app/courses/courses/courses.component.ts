55f25735f04fd7e03d2f55dbf2c86ab50b43f64c
Rafael Alvarenga <rafaelalvarenga_bsi@hotmail.com>
2022-03-15 22:47:55 -0300

    Crud Angular + Spring

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,42 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Course } from './../model/course';
+import { CoursesService } from './../services/courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses$: Observable <Course []>;
+  displayedColumns = ['name','category'];
+
+  constructor(private CoursesService: CoursesService,public dialog: MatDialog) {
+    // this.courses = [];
+    //this.CoursesService = new CoursesService();
+    this.courses$ = this.CoursesService.list()
+    .pipe(
+      catchError(error => {
+      this.onError('Erro ao carregar cursos. ')
+      return of([])
+    })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
